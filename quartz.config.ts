a997cadb1a37bddc3dd6d7134026c83f8cdb2c88
Kiernan Preve <44746791+KierPrev@users.noreply.github.com>
2025-09-09 09:41:57 -0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,6 +86,12 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
+      Plugin.Citations({
+        bibliographyFile: "./content/bib.bib",
+        csl: "ieee",          // o "ieee" (numérico)
+        linkCitations: true,
+        suppressBibliography: false
+      })
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
